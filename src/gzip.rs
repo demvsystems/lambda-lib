032d403fb21392eab31066120044dd70b550b7ff
Sascha <s.scholly@demv.de>
2021-05-03 09:38:13 +0200

    korrekte Methoden angepasst...

--- src/gzip.rs
+++ src/gzip.rs
@@ -7,19 +7,20 @@ pub fn decode(bytes: Vec<u8>) -> Result<String, String> {
 
     match decoder.read_to_string(&mut xml) {
         Ok(_) => Ok(xml),
-        Err(error) => Err(error.to_string())
+        Err(error) => Err(error.to_string()),
     }
 }
 
-pub fn encode(content: &str) -> Result<String, String> {
-    use flate2::read::GzDecoder;
+pub fn encode(content: &str) -> Result<Vec<u8>, String> {
+    use flate2::read::GzEncoder;
+    use flate2::Compression;
     use std::io::Read;
 
-    let mut decoder = GzDecoder::new(&bytes[..]);
-    let mut xml = String::new();
+    let mut encoder = GzEncoder::new(content.as_bytes(), Compression::best());
+    let mut bytes = Vec::new();
 
-    match decoder.read_to_string(&mut xml) {
-        Ok(_) => Ok(xml),
+    match encoder.read_to_end(&mut bytes) {
+        Ok(_) => Ok(bytes),
         Err(error) => Err(error.to_string()),
     }
 }
