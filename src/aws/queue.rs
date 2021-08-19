5e40dca90f3d1f73a812575c1e1de7269452a1a7
C. Jeremiah Schneider <j.schneider@demv.de>
2021-08-19 15:46:15 +0200

    Update Rusoto to 0.47
    The Rusoto update has two major changes from the 0.42 which we were previously using, the first is using the std::futures::Future trait from 0.43 and using tokio from 0.46.

--- src/aws/queue.rs
+++ src/aws/queue.rs
@@ -1,21 +1,21 @@
 use rusoto_sqs::SqsClient;
 
-pub trait Send {
-    fn send_message<S: Into<String>>(&self, message: S, url: S) -> Result<(), String>;
-}
+// pub trait Send {
+//     fn send_message<S: Into<String>>(&self, message: S, url: S) -> Result<(), String>;
+// }
 
-pub trait SerdeSerializedSend {
-    fn send_serialized<T: ?Sized, S: Into<String>>(&self, value: &T, url: S) -> Result<(), String>
-    where
-        T: serde::Serialize + std::fmt::Debug;
-}
+// pub trait SerdeSerializedSend {
+//     fn send_serialized<T: ?Sized, S: Into<String>>(&self, value: &T, url: S) -> Result<(), String>
+//     where
+//         T: serde::Serialize + std::fmt::Debug;
+// }
 
 pub struct Queue {
     pub client: SqsClient,
 }
 
-impl Send for Queue {
-    fn send_message<S: Into<String>>(&self, message: S, url: S) -> Result<(), String> {
+impl Queue {
+    async fn send_message<S: Into<String>>(&self, message: S, url: S) -> Result<(), String> {
         use rusoto_sqs::{SendMessageRequest, Sqs};
 
         let mut req = SendMessageRequest::default();
@@ -23,27 +23,19 @@ impl Send for Queue {
         req.queue_url = url.into();
 
         let future = self.client.send_message(req);
-        match future.sync() {
+        match future.await {
             Ok(_) => Ok(()),
-            Err(e) => Err(format!(
-                "Die Nachricht konnte nicht synchronisiert werden: {:?}",
-                e
-            )),
+            Err(e) => Err(format!("Die Nachricht konnte nicht synchronisiert werden: {:?}", e)),
         }
     }
-}
 
-impl SerdeSerializedSend for Queue {
-    fn send_serialized<T: ?Sized, S: Into<String>>(&self, value: &T, url: S) -> Result<(), String>
-    where
-        T: serde::Serialize + std::fmt::Debug,
+    async fn send_serialized<T: ?Sized, S: Into<String>>(&self, value: &T, url: S) -> Result<(), String>
+        where
+            T: serde::Serialize + std::fmt::Debug,
     {
         match serde_json::to_string(&value) {
-            Ok(json) => self.send_message(json, url.into()),
-            Err(e) => Err(format!(
-                "Das Objekt {:?} konnte nicht serialisiert werden: {:?}",
-                value, e
-            )),
+            Ok(json) => self.send_message(json, url.into()).await,
+            Err(e) => Err(format!("Das Objekt {:?} konnte nicht serialisiert werden: {:?}", value, e)),
         }
     }
 }
