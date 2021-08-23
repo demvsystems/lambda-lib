6ecbee2c68ee4061d3c77252498be9837b7b66de
C. Jeremiah Schneider <j.schneider@demv.de>
2021-08-23 11:02:54 +0200

    fix formatting

--- src/aws/queue.rs
+++ src/aws/queue.rs
@@ -14,17 +14,27 @@ impl Queue {
 
         match self.client.send_message(req).await {
             Ok(_) => Ok(()),
-            Err(e) => Err(format!("Die Nachricht konnte nicht synchronisiert werden: {:?}", e)),
+            Err(e) => Err(format!(
+                "Die Nachricht konnte nicht synchronisiert werden: {:?}",
+                e
+            )),
         }
     }
 
-    pub async fn send_serialized<T: ?Sized, S: Into<String>>(&self, value: &T, url: S) -> Result<(), String>
-        where
-            T: serde::Serialize + std::fmt::Debug,
+    pub async fn send_serialized<T: ?Sized, S: Into<String>>(
+        &self,
+        value: &T,
+        url: S,
+    ) -> Result<(), String>
+    where
+        T: serde::Serialize + std::fmt::Debug,
     {
         match serde_json::to_string(&value) {
             Ok(json) => self.send_message(json, url.into()).await,
-            Err(e) => Err(format!("Das Objekt {:?} konnte nicht serialisiert werden: {:?}", value, e)),
+            Err(e) => Err(format!(
+                "Das Objekt {:?} konnte nicht serialisiert werden: {:?}",
+                value, e
+            )),
         }
     }
 }
